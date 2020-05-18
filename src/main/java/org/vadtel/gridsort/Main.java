ad015545c06fbbbe1a2650b5fe4b97884ddb00b1
Vadim Teltevski <vadim.teltevski@gmail.com>
2020-05-18 17:25:12 +0300

    Solve problem with invalid data

--- src/main/java/org/vadtel/gridsort/Main.java
+++ src/main/java/org/vadtel/gridsort/Main.java
@@ -2,6 +2,7 @@ package org.vadtel.gridsort;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -11,30 +12,48 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         List<List<String>> dataList = readFile(args[0]);
         sort(dataList);
         writeFile(args[1], dataList);
     }
 
-    private static List<List<String>> readFile(String path) throws IOException {
-        List<List<String>> dataList = Files.lines(Paths.get(path))
-                .map(s -> Arrays.asList(s.split("\t")))
-                .collect(Collectors.toList());
+    private static List<List<String>> readFile(String path) {
+        List<List<String>> dataList = null;
+        try {
+            dataList = Files.lines(Paths.get(path))
+                    .map(s -> Arrays.asList(s.split("\t")))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла");
+            System.exit(1);
+        }
+        //Check
+        for (int i = 0; i < dataList.size() - 1; i++) {
+            if (dataList.get(i) != dataList.get(i + 1)) {
+                System.err.println("Данные в файле не вылидны.");
+                System.exit(1);
+            }
+        }
         return dataList;
     }
 
-    private static void writeFile(String path, List<List<String>> sortedData) throws IOException {
-        FileWriter writer = new FileWriter(path);
+    private static void writeFile(String path, List<List<String>> sortedData){
 
-        for (List<String> list : sortedData) {
-            for (String str : list) {
-                writer.write(str + "\t");
+        try (FileWriter writer = new FileWriter(path)){
+            for (List<String> list : sortedData) {
+                for (String str : list) {
+                    writer.write(str + "\t");
+                }
+                writer.write(System.lineSeparator());
             }
-            writer.write(System.lineSeparator());
-
+        } catch (IOException e){
+            System.err.println("Ошибка при записи файла");
+            System.exit(1);
         }
-        writer.close();
+
+
+
     }
 
     private static void sort(List<List<String>> dataList) {
